91a3dc20dbe4cd75650159e30fb6819381795b8b
pony <111l000393@gmail.com>
2017-12-25 17:32:19 +0900

    11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # 渡された文字列のハッシュ値を返す
    def User.digest(string)
@@ -14,4 +16,45 @@ class User < ApplicationRecord
                                                    BCrypt::Engine.cost
      BCrypt::Password.create(string, cost: cost)
    end
- end
+
+   def User.new_token
+     SecureRandom.urlsafe_base64
+   end
+   def remember
+     self.remember_token = User.new_token
+     update_attribute(:remember_digest, User.digest(remember_token))
+   end
+
+   def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+   end
+
+   def forget
+      update_attribute(:remember_digest, nil)
+   end
+
+   def activate
+     update_columns(activated: true, activated_at: Time.zone.now)
+   end
+
+   # 有効化用のメールを送信する
+   def send_activation_email
+     UserMailer.account_activation(self).deliver_now
+   end
+
+
+   private
+
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+end
